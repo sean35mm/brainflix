64b2e2242177b695e5c20a5b23bf0287c6135898
sean35mm <sean35mm@gmail.com>
2021-04-18 02:17:37 -0700

    Added video into public

--- sprint-three/server/routes/videos.js
+++ sprint-three/server/routes/videos.js
@@ -2,6 +2,7 @@ const express = require("express");
 const uuid = require("uuid");
 const fs = require("fs");
 const videoList = require("../data/video-details.json");
+const path = require("path");
 
 const videoRoutes = express.Router();
 
@@ -24,6 +25,35 @@ videoRoutes.get("/:videoId", (req, res) => {
 		return video.id === req.params.videoId;
 	});
 	res.status(200).send(oneVideo);
+});
+
+videoRoutes.post("/", (req, res) => {
+	const aVideo = {
+		id: uuid.v4(),
+		title: req.body.title,
+		channel: "Red Cow",
+		image: "http://localhost:8080/images/Upload-video-preview.jpg",
+		description: req.body.description,
+		views: "1,001,023",
+		likes: "110,985",
+		duration: "4:01",
+		video: "http://localhost:8080/video/BrainStationSampleVideo.mp4",
+		timestamp: Date.now(),
+		comments: [],
+	};
+	// reading json file by readFileSync
+	// const rawdata = fs.readFileSync(
+	//   path.resolve(dirname, "../data/video-details.json")
+	// );
+	// // parse the data into array
+	// const videos = JSON.parse(rawdata);
+	// push new data into array
+	videoList.push(aVideo);
+	// parse into json
+	json = JSON.stringify(videoList);
+	// write back to the file
+	fs.writeFileSync(path.resolve(__dirname, "../data/video-details.json"), json);
+	res.status(201).send(aVideo);
 });
 
 module.exports = videoRoutes;
